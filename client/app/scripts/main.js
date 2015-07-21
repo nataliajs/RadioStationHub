8e45b0a15c44b90d20475e0a08db65138d8431b7
nataliajs <nataliajimenezsanchez@gmail.com>
2015-07-21 19:58:40 +0200

    ajax call, datatables data, modal

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -1,31 +1,54 @@
 'use strict';
 $(document).ready(function(){
-	$('#tabla').DataTable({
+	var datatable=$('#tabla').DataTable({
 		"info":false,
 		"paging":false,
-		//"pagingType":"simple"
+		"ajax":"scripts/jsonRadio.txt",
+		"columns":[
+			{"data":"name"},
+			{
+				"data":"source",
+				"render":function(data){
+					return '<a href="'+data+'" target="_blank" role="button" class="btn btn-warning">Play</a>';
+				}
+			},
+			{
+				"data":"source",
+				"render":function(data){
+					return '<button type="button" class="btn btn-info" id="editBtn">Edit</button><button type="button" class="btn btn-info" id="deleteBtn">Delete</button>';
+				}
+			}
+		]
 	});
 
 
 
 	//show addModal
-	$('#add').click(function(e){
+	$('#addBtn').on("click",function(e){
 		e.preventDefault();
 		document.getElementById('addForm').reset();
 		$('#modalAdd').modal('show');
 	});
 
-	//show addModal
-	$('#edit').click(function(e){
+	//show editModal
+	$('#tabla').on("click",'#editBtn',function(e){
 		e.preventDefault();
+
+		var tr=$(this).parents('tr');
+		var row=datatable.row(tr).data();
+		var name=row.name;
+		var url=row.source;
 		document.getElementById('editForm').reset();
 		$('#modalEdit').modal('show');
+		$('#editName').val(name);
+		$('#editUrl').val(url);
 	});
 
 	//show modalDelete
-	$('#delete').click(function(e){
+	$('#tabla').on("click",'#deleteBtn',function(e){
 		e.preventDefault();
-		document.getElementById('editForm').reset();
+		
 		$('#modalDelete').modal('show');
+
 	});
 });
